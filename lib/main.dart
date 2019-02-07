f094e04f08200c0d872be054bb8ec46f2f70ea2c
SERGIO MONJARAZ <SERGIOMONJARAZ@MacBook-Air-de-Andrea.local>
2019-02-07 10:14:47 -0600

    Primer Commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:beta_balmer/pages/login_page.dart';
+import 'package:beta_balmer/pages/notfound_page.dart';
+import 'package:beta_balmer/utils/uidata.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+   
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  final materialApp = MaterialApp(
+      title: UIData.appName,
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          fontFamily: UIData.quickFont,
+          primarySwatch: Colors.amber),
+      debugShowCheckedModeBanner: false,
+      showPerformanceOverlay: false,
+      home: LoginPage(),
+      supportedLocales: [
+        const Locale("en", "US"),
+        const Locale("hi", "IN"),
+      ],
+      // initialRoute: UIData.notFoundRoute,
+
+      //routes
+      routes: <String, WidgetBuilder>{
+        UIData.loginRoute: (BuildContext context) => LoginPage(),
+      
+      },
+      onUnknownRoute: (RouteSettings rs) => new MaterialPageRoute(
+          builder: (context) => new NotFoundPage(
+                appTitle: UIData.coming_soon,
+                icon: FontAwesomeIcons.solidSmile,
+                title: UIData.coming_soon,
+                message: "Under Development",
+                iconColor: Colors.green,
+              )));
+  // This widget is the root of your application.
+
+
+  @override
+  Widget build(BuildContext context) {
+
+      return materialApp;
+
+  }
+}
+
+
