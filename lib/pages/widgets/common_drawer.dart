ce7c2a817b34c59d74468fb89b9e3a231a0f37b6
alan140703 <47754932+alan140703@users.noreply.github.com>
2019-03-09 21:35:15 -0600

    Cambiar colores, columnas y botones

--- lib/pages/widgets/common_drawer.dart
+++ lib/pages/widgets/common_drawer.dart
@@ -7,84 +7,135 @@ class CommonDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-
         padding: EdgeInsets.zero,
         children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text(
-              "¿Donde te encuentras?",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22.0),
+          Row(
+            children: <Widget>[
+              Expanded(
+               child: UserAccountsDrawerHeader(
+                 decoration: BoxDecoration(
+                   color: Colors.lightBlue,
+                 ),
+                 currentAccountPicture: new CircleAvatar(
+                   backgroundColor: Colors.transparent,
+                   backgroundImage: new AssetImage(UIData.balmerLogo),
+                  
+                 ),
+                  accountName: Text(
+                    "¿Donde te encuentras?",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22.0, color: Colors.black),
+                  ),
+                  accountEmail: Text(
+                    "Guadalajara, Mèxico",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              )
+            ],
+          ),
+          
+         //HAcer botones completos
+            Row(
+          children: <Widget>[
+            Expanded(
+            child:new ListTile (
+              title: Text(
+            "Imformaciòn de usuario",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
             ),
-            accountEmail: Text(
-              "Guadalajara, Mèxico",
+                leading: IconButton(
+                  icon: Icon(Icons.contact_mail),
+                  color: Colors.black,
+                  onPressed: (){
+                    Navigator.pushNamed(context, UIData.activityRoute1);
+                  },
+                ),
             ),
+            )
+           ],
+          ),
+          Row(
+          children: <Widget>[
+            Expanded(
+           child:
+            new ListTile(
 
-            currentAccountPicture: new CircleAvatar(
-              backgroundImage: new AssetImage(UIData.balmerLogo),
-            ),
-          ),
-          
-          //hacer esto un bonton
-          new ListTile (
+              leading: IconButton(
+                icon: Icon(Icons.star_border),
+                color: Colors.black,
+                onPressed: (){
+                  Navigator.pushNamed(context, UIData.activityRoute1);
+                },
+              ),
             title: Text(
-              "Imformaciòn de usuario",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
+            "Apreciacion",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
             ),
-            leading: Icon(
-              Icons.contact_mail,
-              color:  Colors.black,
             ),
+            ),
+          ]
           ),
-          
-           //hacer esto un bonton
-          new ListTile(
-            title: Text(
-              "Apreciacion",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
-            ),
-            leading: Icon(
-              Icons.star_border,
-              color: Colors.black,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child:
+                new ListTile(
+                  title: Text(
+                    "Estadìsticas / Pagos",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
+                  ),
+                  leading: IconButton(
+                    icon: Icon(Icons.credit_card),
+                    color: Colors.black,
+                    onPressed: (){
+                      Navigator.pushNamed(context, UIData.activityRoute1);
+                    },
+                  ),
+                ),
+              )
+            ],
           ),
-          
-           //hacer esto un bonton
-          new ListTile(
-            title: Text(
-              "Estadìsticas / Pagos",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
-            ),
-            leading: Icon(
-              Icons.credit_card,
-              color: Colors.black,
-            ),
+
+          Row(
+            children: <Widget>[
+              Expanded(
+                child:
+                new ListTile(
+                  title: Text(
+                    "Configuraciòn",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
+                  ),
+                  leading: IconButton(
+                    icon: Icon(Icons.settings),
+                    color: Colors.black,
+                    onPressed: (){
+                      Navigator.pushNamed(context, UIData.activityRoute1);
+                    },
+                  ),
+                ),
+              )
+            ],
           ),
-          
-           //hacer esto un bonton
-          new ListTile(
-            title: Text(
-              "Configuraciòn",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
-            ),
-            leading: Icon(
-              Icons.settings,
-              color: Colors.black,
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child:
+                new ListTile(
+                  title: Text(
+                    "Ayuda",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
+                  ),
+                  leading: IconButton(
+                    icon: Icon(Icons.help_outline),
+                    color: Colors.black,
+                    onPressed: (){
+                      Navigator.pushNamed(context, UIData.activityRoute1);
+                    },
+                  ),
+                ),
+              )
+            ],
           ),
-          Divider(),
-          
-           //hacer esto un bonton
-          new ListTile(
-            title: Text(
-              "Ayuda",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
-            ),
-            leading: Icon(
-              Icons.help_outline,
-              color: Colors.black,
-            ),
-          ),
-          Divider(),
           //MyAboutTile()
         ],
       ),
